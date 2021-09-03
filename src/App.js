36452f2062decedfd2186fb8e927c555a2959061
Guilherme <guidumasperes@gmail.com>
2021-09-02 21:32:58 -0300

    Adding React.js project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
+import restaurant from './restaurant.jpg';
+
+/* What is a component ?
+A component is a little piece of the UI that we are going to use 
+to describe one part of our application.
+
+We create a component by creating a function, the function returns JSX
+
+Every react component has access to this object called "props"
+They hold all different properties for our component
+To add values to a component(his props) we need to add them to the place where the
+component is being rendered
+
+If we are passing a variable to props we enclose it with curly braces
+Quotes are for strings
+*/
+
+function Header(props) {
+  console.log(props);
+  return(
+    <header>
+      <h1>{props.name}'s Kitchen</h1>
+    </header>
+  );
+}
+
+function Main(props) {
+  return(
+    <section>
+      <p>We serve the most {props.adjective} food around!</p>
+      <img src={restaurant} height={200} alt="napkin and fork at a restaurant table"/>
+      <ul style={{textAlign: "left"}}>
+        {props.dishes.map((dish) => (
+          <li key={dish.id}> {dish.title} </li>
+        ))}
+      </ul>
+    </section>
+  );
+}
+
+function Footer(props) {
+  return(
+    <footer>
+      <p>Copyright {props.year}</p>
+    </footer>
+  );
+}
+
+//We need to adjust this a little bit to avoid errors with keys in react
+const dishes = [
+  "Ribs with bacon",
+  "Hamburguer",
+  "Beef with grilled cheese"
+];
+//Lets transform dishes in objs 
+const dishesObjs = dishes.map((dish, i) => ({id:i, title:dish}));
+//Keys can help to keep you data sync as the application changes over time 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header name="Gordon"/>
+      <Main adjective="amazing" dishes={dishesObjs}/>
+      <Footer year={new Date().getFullYear()}/>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from "./App";
+
+function AppTwo() {
+  return <h1>This is the Second App</h1>;
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  /*This React.createElement() function creates an element
+  first param is the element we want to create
+  second is any properties we want
+  the last is the content of the element
+  */
+  React.createElement("h1", 
+  {style: {color:"blue"}}, 
+  "Hello React!"),
   document.getElementById('root')
-);
+); 
+/*The first parameter of this function is the element we want to create
+  The second is where we want to put it(in what part of the html page)
+*/
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(
+  <ul>
+    <li>Cat</li>
+    <li>Dog</li>
+    <li>Fish</li>
+  </ul>,
+  document.getElementById('jsx-test')
+);
+//The first argument of this function above is what is called: JSX
+
+ReactDOM.render(
+  <React.Fragment>
+    <App/>
+    <AppTwo/>
+  </React.Fragment>,
+  document.getElementById('app-component')
+);
+/*
+We only can render one component, we can't render tow sibling componenets unless they're wrapped
+in some sort of another tag(like "div" )
+Too solve this we can wrap the content in a React.Fragment
+You can also, only use <> ... </> as a shorthand to React.Fragment
+*/
